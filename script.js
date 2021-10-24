c00dfee286868e2fbcf0358aeb12482f93de78e8
Lorenzo Ercoli <l.ercoli@qzrstudio.com>
2021-10-24 15:33:20 +0200

    Fluxo-online

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	
+	try {
+		$('main').ripples({
+			resolution: 1500,
+			dropRadius: 15, //px
+			perturbance: 0.05,
+			strength: 1,
+		});
+	}
+
+	catch (e) {
+		$('.error').show().text(e);
+	}
+
+	setInterval(function() {
+		var $el = $('main');
+		var x = Math.random() * $el.outerWidth();
+		var y = Math.random() * $el.outerHeight();
+		var dropRadius = 15;
+		var strength = 0.004 + Math.random() * 0.004;
+
+		$el.ripples('drop', x, y, dropRadius, strength);
+	}, 1000);
+
+
+});
